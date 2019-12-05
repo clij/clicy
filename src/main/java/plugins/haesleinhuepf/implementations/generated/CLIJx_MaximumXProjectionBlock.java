c607dd18aaa32e28a10707383440b1faf6490b60
haesleinhuepf <rhaase@mpi-cbg.de>
2019-12-05 20:05:46 +0100

    regenerated blocks

---
+++ src/main/java/plugins/haesleinhuepf/implementations/generated/CLIJx_MaximumXProjectionBlock.java
@@ -0,0 +1,14 @@
+package plugins.haesleinhuepf.implementations.generated;
+import plugins.haesleinhuepf.AbstractCLIJxBlock;
+import net.haesleinhuepf.clijx.advancedfilters.MaximumXProjection;
+// this is generated code. See src/test/java/net/haesleinhuepf/clicy/codegenerator for details
+public class CLIJx_MaximumXProjectionBlock extends AbstractCLIJxBlock {
+   
+    /**
+     * Determines the maximum projection of an image along X.
+     */
+    public CLIJx_MaximumXProjectionBlock() {
+        super(new MaximumXProjection());
+    }
+
+}
---
+++ src/main/java/plugins/haesleinhuepf/implementations/generated/CLIJx_MaximumYProjectionBlock.java
@@ -0,0 +1,14 @@
+package plugins.haesleinhuepf.implementations.generated;
+import plugins.haesleinhuepf.AbstractCLIJxBlock;
+import net.haesleinhuepf.clijx.advancedfilters.MaximumYProjection;
+// this is generated code. See src/test/java/net/haesleinhuepf/clicy/codegenerator for details
+public class CLIJx_MaximumYProjectionBlock extends AbstractCLIJxBlock {
+   
+    /**
+     * Determines the maximum projection of an image along X.
+     */
+    public CLIJx_MaximumYProjectionBlock() {
+        super(new MaximumYProjection());
+    }
+
+}
