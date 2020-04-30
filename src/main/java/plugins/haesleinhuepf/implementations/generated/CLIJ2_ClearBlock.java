b4848f1cab78c7efb315c9895a300b8477f14942
haesleinhuepf <rhaase@mpi-cbg.de>
2020-04-30 11:49:50 +0200

    added clear blocks

---
+++ src/main/java/plugins/haesleinhuepf/implementations/generated/CLIJ2_ClearBlock.java
@@ -0,0 +1,14 @@
+package plugins.haesleinhuepf.implementations.generated;
+import plugins.haesleinhuepf.AbstractCLIJ2Block;
+import net.haesleinhuepf.clij2.plugins.Clear;
+// this is generated code. See src/test/java/net/haesleinhuepf/clicy/codegenerator for details
+public class CLIJ2_ClearBlock extends AbstractCLIJ2Block {
+   
+    /**
+     * Resets the GPUs memory by deleting all cached images.
+     */
+    public CLIJ2_ClearBlock() {
+        super(new Clear());
+    }
+
+}
